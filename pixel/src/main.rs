aaf319f150109440a1face96d1a246821b8c4b51
Stephen Hicks <sdh33@cornell.edu>
2015-11-16 12:33:26 -0800

    add solution to wsj pixel problem

---
+++ pixel/src/main.rs
@@ -0,0 +1,334 @@
+use std::collections::{HashMap,HashSet};
+use std::hash::Hash;
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::ops::{Index, IndexMut};
+
+macro_rules! do_option {
+    ($x:expr) => (match $x {
+        None => { return None; },
+        Some(x) => x
+    })
+}
+
+// fn minimum_by<T, F>(v: Vec<T>, f: F) -> Option<T> where F: Fn(&T) -> usize {
+//     let mut best_score = 0;
+//     let mut best_item: Option<T> = None;
+//     for item in v.drain(..) {
+//         let score = f(item);
+//         if best_item.is_none() || score < best_score {
+//             best_score = score;
+//             best_item = Some(item);
+//         }
+//     }
+//     best_item
+// }
+
+// Iterates over pairs in a principled way.
+struct Pairs {
+    start: usize,
+    x: usize,
+    y: usize,
+}
+
+impl Pairs {
+    fn starting_at(n: usize) -> Pairs {
+        Pairs{start: n, x: n, y: n}
+    }
+}
+
+impl Iterator for Pairs {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<(usize, usize)> {
+        let result = (self.x, self.y);
+        if self.x == self.start {
+            self.x += self.y - self.start + 1;
+            self.y = self.start;
+        } else {
+            self.x -= 1;
+            self.y += 1;
+        }
+        Some(result)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
+    }
+}
+
+/// Iterates over multiple ranges at once
+struct Vecs<'a> {
+    start: usize,
+    ends: &'a Vec<usize>,
+    pos: usize,
+}
+
+impl<'a> Iterator for Vecs<'a> {
+    type Item = Vec<usize>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Vec<usize>> {
+        let mut out = vec![];  // TODO - preinitialize capacity?
+        let mut num = self.pos;
+        self.pos += 1;
+        for end in self.ends {
+            let div = end - self.start;
+            if div == 0 {
+                return None;
+            }
+            out.push(self.start + (num % div));
+            num /= div;
+        }
+        if num > 0 {
+            None
+        } else {
+            Some(out)
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let mut total = 1;
+        for end in self.ends {
+            total *= end - self.start;
+        }
+        let left = total - self.pos;
+        (left, Some(left))
+    }
+}
+
+impl<'a> Vecs<'a> {
+    fn start(start: usize, ends: &'a Vec<usize>) -> Vecs<'a> {
+        Vecs{start: start, ends: ends, pos: 0}
+    }
+}
+
+#[derive(Clone,PartialEq,Eq,Hash)]
+struct Counts {
+    rows: Vec<usize>,
+    cols: Vec<usize>,
+}
+
+impl Counts {
+    fn size(&self) -> usize {
+        let mut c: usize = 0;
+        for row in &self.rows {
+            c += *row;
+        }
+        c
+    }
+
+    fn non_trivial(&self) -> bool {
+        for row in &self.rows {
+            if *row == 0 || *row == self.cols.len() { return false; }
+        }
+        for col in &self.cols {
+            if *col == 0 || *col == self.rows.len() { return false; }
+        }
+        true
+    }
+}
+
+impl Display for Counts {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for row in &self.rows {
+            s.push_str(&format!("{} ", row));
+        }
+        s.push('|');
+        for col in &self.cols {
+            s.push_str(&format!(" {}", col));
+        }
+        write!(f, "{}", s)
+    }
+}
+
+struct Pixel {
+    values: Vec<bool>,
+    width: usize,
+    height: usize,
+}
+
+impl Index<(usize, usize)> for Pixel {
+    type Output = bool;
+
+    fn index(&self, index: (usize, usize)) -> &bool {
+        let (x, y) = index;
+        &self.values[y * self.width + x]
+    }
+}
+
+impl IndexMut<(usize, usize)> for Pixel {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut bool {
+        let (x, y) = index;
+        &mut self.values[y * self.width + x]
+    }
+}
+
+impl Display for Pixel {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let mut s = String::new();
+        s.push('+');
+        for _ in 0..self.width { s.push('-'); }
+        s.push_str("+\n");
+        for y in 0..self.height {
+            s.push('|');
+            for x in 0..self.width {
+                s.push(if self[(x, y)] { '*' } else { ' ' });
+            }
+            s.push_str("|\n");
+        }
+        s.push('+');
+        for _ in 0..self.width { s.push('-'); }
+        s.push('+');
+        write!(f, "{}", s)
+    }
+}
+
+impl Pixel {
+    fn new(width: usize, height: usize) -> Pixel {
+        Pixel{values: vec![false; width * height], width: width, height: height}
+    }
+
+    fn count<F>(&self, max: usize, getter: F) -> Option<usize> 
+        where F : Fn(usize) -> bool {
+        let mut count: isize = 0;
+        let mut result = Some(0);
+        for z in (0..max) {
+            match (getter(z), count) {
+                (true, -1) => return None, // multiple groups
+                (true, _) => {
+                    count += 1;
+                    result = Some(count as usize);
+                },
+                (false, 0) | (false, -1) => (),
+                (false, _) => count = -1,
+            }
+        }
+        result
+    }
+
+    fn row(&self, y: usize) -> Option<usize> {
+        self.count(self.width, |x| { self[(x, y)] })
+    }
+
+    fn col(&self, x: usize) -> Option<usize> {
+        self.count(self.height, |y| { self[(x, y)] })
+    }
+
+    fn counts(&self) -> Option<Counts> {
+        let mut rows = vec![];
+        let mut cols = vec![];
+        for x in 0..self.width {
+            cols.push(do_option!(self.col(x)));
+        }
+        for y in 0..self.height {
+            rows.push(do_option!(self.row(y)));
+        }
+        Some(Counts{rows: rows, cols: cols})
+    }
+}
+
+// #[derive(PartialEq,Eq)]
+// struct Key<K>(RefCell<K>);
+
+// impl<K: Hash> Hash for Key<K> {
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self[0].borrow().hash(state);
+//     }
+// }
+
+struct UniqueMap<K, V> where K: Eq+Hash {
+    doubles: HashSet<K>,
+    data: HashMap<K, V>,
+}
+
+impl<K: Clone+Eq+Hash, V> UniqueMap<K, V> {
+    fn new() -> UniqueMap<K, V> {
+        UniqueMap{doubles: HashSet::new(), data: HashMap::new()}
+    }
+
+    fn put(&mut self, key: K, value: V) {
+        if self.data.contains_key(&key) {
+            self.data.remove(&key);
+            self.doubles.insert(key);
+        } else if !self.doubles.contains(&key) {
+            self.data.insert(key, value);
+        }
+    }
+
+    fn drain_unique(&mut self) -> Vec<(K, V)> {
+        let mut keys: Vec<K> = vec![];
+        {
+            for key in self.data.keys() {
+                keys.push(key.clone());
+            }
+        }
+        keys.iter().map(|k| (k.clone(), self.data.remove(k).unwrap())).collect()
+    }
+}
+
+fn find() -> Pixel {
+    for (width, height) in Pairs::starting_at(1) {
+        let mut map: UniqueMap<Counts, Pixel> = UniqueMap::new();
+        // TODO(sdh): if we're smarter about generating these, then
+        // we can save lots of work...
+
+        let limits = vec![width; height];
+        for counts in Vecs::start(1, &limits) {
+            let max_starts: Vec<usize> =
+                counts.iter().map(|count| { width - count + 1 }).collect();
+            for starts in Vecs::start(0, &max_starts) {
+                // Build up the board
+                let mut pix = Pixel::new(width, height);
+                for row in 0..height {
+                    for col in 0..counts[row] {
+                        pix[(col + starts[row], row)] = true;
+                    }
+                }
+                match pix.counts() {
+                    Some(c) => {
+                        if c.non_trivial() { map.put(c, pix) };
+                    },
+                    None => (),
+                }
+            }
+        }
+
+        if !map.data.is_empty() {
+            // for (k, v) in map.data.iter() {
+            //     println!("{}\n{}", v, k);
+            // }
+            let mut best_count = 0;
+            let mut best: Option<Pixel> = None;
+            for (k, v) in map.drain_unique() {
+                if best.is_none() || k.size() < best_count {
+                    best_count = k.size();
+                    best = Some(v);
+                }
+            }
+            return best.unwrap();
+            // {
+            //     key = map.data.keys().next().unwrap().clone();
+            // }
+            // return map.data.remove(key).unwrap();
+        }
+        
+    }
+    panic!("Impossible")
+}
+
+
+fn main() {
+    // let mut p = Pixel::new(5, 5);
+    // p[(2, 3)] = true;
+    // p[(1, 1)] = true;
+    // p[(4, 0)] = true;
+    let p = find();
+    println!("{}", p);
+    match p.counts() {
+        Some(c) => println!("{}", c),
+        None => println!("non-unique"),
+    }
+}
--- stick/src/main.rs
+++ stick/src/main.rs
@@ -1,7 +1,61 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashSet;
 use std::fmt;
 use std::vec::Vec;
 
-#[derive(Debug)]
+// struct Pairs<A, T> {
+//     orig: T,
+//     outer: T,
+//     inner: T,
+//     last: Some<A>,
+//     read: usize,
+// }
+
+// impl<A, T> Iterator for Pairs<A, T> where T: Clone + Iterator<Item = A> {
+//     type Item = (A, A);
+
+//     #[inline]
+//     fn next(&mut self) -> Option<(A, A)> {
+//         match self.last {
+//             None => return None,
+//             _ => (),
+//         }
+//         match self.inner.next() {
+//             Some(x) => {
+//                 self.read = self.read + 1;
+//                 Some((self.last, x))
+//             },
+//             None => {
+//                 self.last = self.outer.next();
+//                 self.inner = self.orig.clone();
+//                 self.next()
+//             },
+//         }
+//     }
+
+//     #[inline]
+//     fn size_hint(&self) -> (usize, Option<usize>) {
+//         let (lower, upper) = self.orig.size_hint();
+//         let lower = lower * lower - read;
+//         let upper = upper.and_then(|x| x * x - read);
+//         (lower, upper)
+//     }
+// }
+
+// fn pairs<T: Clone + Iterator>(iter: &T) -> Iterator<(T::Item, T::Item)> {
+//     Pairs{
+//         orig: iter,
+//         outer: iter.clone(),
+//         inner: iter.clone(),
+//         last: None,
+//         read: 0,
+//     }
+// }
+
+
+#[derive(Debug,PartialEq,Eq,Clone,Hash)]
 struct Pos {
     x: usize,
     y: usize,
@@ -24,6 +78,24 @@ impl Pos {
             false
         }
     }
+
+    fn third(&self, other: &Pos) -> Option<Pos> {
+        if self == other {
+            return None;
+        }
+        let dx = self.x as i8 - other.x as i8;
+        let dy = self.y as i8 - other.y as i8;
+        if dx.abs() > 1 || dy.abs() > 1 {
+            return None;
+        }
+        let x = self.x as i8 + dx;
+        let y = self.y as i8 + dy;
+        if x < 0 || x > 3 || y < 0 || y > 3 {
+            return None;
+        }
+        //println!("third({}, {}) => ({}, {})", self, other, x, y);
+        Some(Pos::new(x as u8, y as u8))
+    }
 }
 
 impl fmt::Display for Pos {
@@ -32,7 +104,7 @@ impl fmt::Display for Pos {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 struct Piece {
     x: Pos,
     o: Pos,
@@ -46,30 +118,119 @@ impl Piece {
         Piece{x: x, o: o}
     }
 
-    fn add(&self, board: &mut Vec<char>) {
+    fn add(&self, board: &mut Vec<char>, turn: char) {
         board[self.x.y * 16 + self.x.x * 2] = 'X';
         board[self.o.y * 16 + self.o.x * 2] = 'O';
+        // TODO(sdh): add turn number instead of = or ‖
         if self.x.x == self.o.x { // vertical
-            board[(self.x.y + self.o.y) * 8 + self.x.x * 2] = '‖';
+            board[(self.x.y + self.o.y) * 8 + self.x.x * 2] = turn; // '‖';
         } else {
-            board[self.x.y * 16 + self.x.x + self.o.x] = '=';
+            board[self.x.y * 16 + self.x.x + self.o.x] = turn; // '=';
         }
     }
+
+    // fn intersects(&self, other: &Piece) -> bool {
+    //     self.x == other.x || self.x == other.o ||
+    //         self.o == other.x || self.o == other.o
+    // }
 }
 
-#[derive(Debug)]
+lazy_static! {
+    static ref ALL_POSITIONS: Vec<Pos> = {
+        let mut all: Vec<Pos> = vec![];
+        for x in 0..4 {
+            for y in 0..4 {
+                all.push(Pos::new(x, y));
+            }
+        }
+        all
+    };
+}
+
+lazy_static! {
+    static ref ALL_PIECES: Vec<Piece> = {
+        let mut all: Vec<Piece> = vec![];
+        let pos: &Vec<Pos> = &ALL_POSITIONS;
+        for x in pos {
+            for o in pos {
+                if x.connected(o) {
+                    all.push(Piece::new(x.clone(), o.clone()));
+                }
+            }
+        }
+        all
+    };
+}
+
+#[derive(Debug,Clone)]
 struct Board {
+    // TODO: Make Board persistent/immutable?!?
+    // TODO: Might be nice to reuse the Pos instances, rather than clone?
     pieces: Vec<Piece>,
+    xs: HashSet<Pos>,
+    os: HashSet<Pos>,
 }
 
 impl Board {
     fn new() -> Board {
-        Board{pieces: vec![]}
+        Board{pieces: vec![], xs: HashSet::new(), os: HashSet::new()}
     }
 
-    fn add(&mut self, p: Piece) {
-        self.pieces.push(p);
+    fn empty(&self, p: &Pos) -> bool {
+        !self.xs.contains(p) && !self.os.contains(p)
     }
+
+    /// Adds the given piece to the board.  Returns true move was legal.
+    fn add(&mut self, p: Piece) -> bool {
+        if !self.empty(&p.x) || !self.empty(&p.o) {
+            return false;
+        }
+        self.xs.insert(p.x.clone());
+        self.os.insert(p.o.clone());
+        self.pieces.push(p);
+        true
+    }
+
+    /// Returns the result, which encodes both the winner (in
+    /// the sign: positive for player 1, negative for player
+    /// 2) and the length of the game (1 + number of blank
+    /// spaces in the magnitude).  If the game is a draw or
+    /// not yet decided, the result is zero.  Player 1 wants
+    /// to maximize this, while player 2 wants to minimize it.
+    fn score(&self) -> i8 {
+        let x_wins = three_in_a_row(&self.xs);
+        let o_wins = three_in_a_row(&self.os);
+        let rest = 9 - self.pieces.len() as i8;
+        if x_wins && o_wins {
+            if rest % 2 == 0 {
+                rest
+            } else {
+                -rest
+            }
+        } else if x_wins {
+            rest
+        } else if o_wins {
+            -rest
+        } else {
+            0
+        }
+    }
+}
+
+fn three_in_a_row(set: &HashSet<Pos>) -> bool {
+    for p1 in set {
+        for p2 in set {
+            match p1.third(p2) {
+                Some(p3) => {
+                    if set.contains(&p3) {
+                        return true;
+                    }
+                },
+                _ => (),
+            }
+        }
+    }
+    false
 }
 
 static EMPTY_BOARD: &'static str =
@@ -78,8 +239,10 @@ static EMPTY_BOARD: &'static str =
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut board: Vec<char> = EMPTY_BOARD.chars().collect();
+        let mut turn = '1';
         for piece in &self.pieces {
-            piece.add(&mut board);
+            piece.add(&mut board, turn);
+            turn = (turn as u8 + 1) as char;
         }
         let mut out: String = String::new();
         for ch in board {
@@ -90,9 +253,40 @@ impl fmt::Display for Board {
     }
 }
 
+fn play(b: Board, goal: i8) -> Board {
+    let pieces: &Vec<Piece> = &ALL_PIECES;
+    let mut boards: Vec<Board> = vec![];
+
+    for p in pieces {
+        let mut b2 = b.clone();
+        if b2.add(p.clone()) {
+            let s2 = b2.score();
+            if s2 * goal > 0 {
+                println!("WIN {}\n{}\n", goal, &b2);
+                return b2;
+            } else if s2 == 0 {
+                boards.push(b2);
+            }
+        }
+    }
+
+    let mut best_score = -100;
+    let mut best_board = b.clone(); // ?!?
+    for board in boards {
+        let board = play(board, -goal);
+        let s = board.score();
+        if s * goal > best_score {
+            best_score = s * goal;
+            best_board = board;
+        }
+    }
+    best_board
+}
+
+
+
+
 fn main() {
-    let mut board = Board::new();
-    board.add(Piece::new(Pos::new(0, 0), Pos::new(0, 1)));
-    board.add(Piece::new(Pos::new(0, 2), Pos::new(1, 2)));
-    println!("{}", board);
+    let board = play(Board::new(), 1);
+    println!("\nOPTIMUM:\n{}", board);
 }
