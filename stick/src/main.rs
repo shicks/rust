379adeef06f239da705075fbdf5f611b0e0e6bc0
Stephen Hicks <sdh33@cornell.edu>
2015-11-16 22:40:20 -0800

    allow reversing permutations (maybe?)

--- stick/src/main.rs
+++ stick/src/main.rs
@@ -100,6 +100,11 @@ impl Pos {
         //println!("third({}, {}) => ({}, {})", self, other, x, y);
         Some(Pos::new(x as u8, y as u8))
     }
+
+    fn permute(&self, perm: &Permutation) -> Pos {
+        let ind = perm.apply(self.index());
+        Pos{y: ind / 4, x: ind % 4}
+    }
 }
 
 impl fmt::Display for Pos {
@@ -137,6 +142,10 @@ impl Piece {
     //     self.x == other.x || self.x == other.o ||
     //         self.o == other.x || self.o == other.o
     // }
+
+    fn permute(&self, perm: &Permutation) -> Piece {
+        Piece{x: self.x.permute(perm), o: self.o.permute(perm)}
+    }
 }
 
 lazy_static! {
@@ -166,17 +175,31 @@ lazy_static! {
     };
 }
 
-type Permutation = [usize;16];
+struct Permutation([usize;16]);
+
+impl Permutation {
+    fn apply(&self, i: usize) -> usize {
+        self.0[i]
+    }
+    fn reverse(&self) -> Permutation {
+        // Most of these are self-inverting, but oh well
+        let mut result = [0;16];
+        for (i, x) in self.0.iter().enumerate() {
+            result[*x] = i;
+        }
+        Permutation(result)
+    }
+}
 
 const PERMUTATIONS: [Permutation;8] = [
-    [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-    [3,7,11,15,2,6,10,14,1,5,9,13,0,4,8,12],
-    [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],
-    [12,8,4,0,13,9,5,1,14,10,6,2,15,11,7,3],
-    [3,2,1,0,7,6,5,4,11,10,9,8,15,14,13,12],
-    [15,11,7,3,14,10,6,2,13,9,5,1,12,8,4,0],
-    [12,13,14,15,8,9,10,11,4,5,6,7,0,1,2,3],
-    [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15],
+    Permutation([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]),
+    Permutation([3,7,11,15,2,6,10,14,1,5,9,13,0,4,8,12]),
+    Permutation([15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]),
+    Permutation([12,8,4,0,13,9,5,1,14,10,6,2,15,11,7,3]),
+    Permutation([3,2,1,0,7,6,5,4,11,10,9,8,15,14,13,12]),
+    Permutation([15,11,7,3,14,10,6,2,13,9,5,1,12,8,4,0]),
+    Permutation([12,13,14,15,8,9,10,11,4,5,6,7,0,1,2,3]),
+    Permutation([0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]),
     ];
 
 #[derive(Debug,Clone)]
@@ -233,24 +256,30 @@ impl Board {
         }
     }
 
-    fn state(&self) -> String {
+    fn state(&self) -> (String, usize) {
         let mut pos = vec![' ';16];
         for piece in &self.pieces {
             pos[piece.x.index()] = 'x';
             pos[piece.o.index()] = 'o';
         }
-        let mut result: Vec<String> = PERMUTATIONS.iter().map(|permutation| {
+        let mut result: Vec<(String, usize)> = vec![];
+        for (num, permutation) in PERMUTATIONS.iter().enumerate() {
             let mut s = String::with_capacity(16);
             for i in 0..16 {
-                s.push(pos[permutation[i]])
+                s.push(pos[permutation.apply(i)])
             }
-            s
-        }).collect();
+            result.push((s, num));
+        }
         result.sort();
-        // TODO(sdh): Store which permutation this was, so that we
-        // can map *back* to the original as needed.  This would need
-        // to be returned separate to the string itself.
         result[0].clone()
+    }
+
+    fn permute(&self, perm: &Permutation) -> Board {
+        let mut result = Board::new();
+        for piece in &self.pieces {
+            result.add(piece.permute(perm));
+        }
+        result
     }
 }
 
@@ -300,9 +329,11 @@ impl Solver {
     fn solve(&mut self, b: Board, goal: i8) -> (Board, i8) {
         let pieces: &Vec<Piece> = &ALL_PIECES;
         let mut boards: Vec<Board> = vec![];
-        let state = b.state();
+        let (state, perm) = b.state();
         match self.0.get(&state) {
-            Some(&(ref b1, s1)) => return (b1.clone(), s1),
+            Some(&(ref b1, s1)) => {
+                return (b1.permute(&PERMUTATIONS[perm].reverse()), s1);
+            },
             None => (),
         }
 
